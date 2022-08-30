d76d3a5fbd67de130b1d261a2dd1a5447c958143
DarioAlessioR <100212928+DarioAlessioR@users.noreply.github.com>
2022-08-30 13:30:19 -0400

    Add JOIN table medical_histories_and_treatments and add foreign key constraints

--- schema_based_on_diagram.sql
+++ schema_based_on_diagram.sql
@@ -1,6 +1,8 @@
 -- Create a database based on a diagram
 -- Create a file named schema_based_on_diagram.sql where you implement the database from the diagram.
 
+CREATE DATABASE clinic;
+
 CREATE TABLE patients(
    id INT PRIMARY KEY NOT NULL GENERATED ALWAYS AS IDENTITY (START WITH 1 INCREMENT BY 1),
    name VARCHAR(50),
@@ -12,7 +14,8 @@ CREATE TABLE medical_histories(
    name VARCHAR(50),
    admitted_at TIMESTAMP,
    patient_id INT,
-   status VARCHAR(50)
+   status VARCHAR(50),
+   CONSTRAINT fk_medical_histories_patients FOREIGN KEY(patient_id) REFERENCES patients(id)
 );
 
 CREATE TABLE treatments(
@@ -26,7 +29,8 @@ CREATE TABLE invoices(
    total_amount DECIMAL,
    generated_at TIMESTAMP,
    payed_at TIMESTAMP,
-   medical_history_id INT
+   medical_histories_id INT,
+   CONSTRAINT fk_invoices_medical_histories FOREIGN KEY(medical_histories_id) REFERENCES medical_histories(id)
 );
 
 CREATE TABLE invoice_items(
@@ -35,5 +39,18 @@ CREATE TABLE invoice_items(
    quantity INT NOT NULL,
    total_price DECIMAL,
    invoice_id INT NOT NULL,
-   treatment_id INT 
+   treatments_id INT,
+   CONSTRAINT fk_invoices_items_invoices FOREIGN KEY(invoice_id) REFERENCES invoices(id),
+   CONSTRAINT fk_invoices_items_treatments FOREIGN KEY(treatments_id) REFERENCES treatments(id)
 );
+
+-- Create an intermmediate table for medical_histories and treatments
+
+CREATE TABLE medical_histories_and_treatments(
+   medical_histories_id INT NOT NULL,
+   treatments_id INT NOT NULL,
+   CONSTRAINT fk_medical_histories FOREIGN KEY(medical_histories_id) REFERENCES medical_histories(id),
+   CONSTRAINT fk_treatments FOREIGN KEY(treatments_id) REFERENCES treatments(id),
+   PRIMARY KEY (medical_histories_id, treatments_id)
+);
+
